5b0f6edfffcf3464ed4bdf37d8750b749d3d61e4
yoichi-project01 <setoyama.yoichi@gmail.com>
2025-05-01 16:50:29 +0900

    ファイルを選択してアップロードを作成

--- mbs/app/Home/CustomerList/components/Modal.tsx
+++ mbs/app/Home/CustomerList/components/Modal.tsx
@@ -12,6 +12,7 @@ const Modal = (props: ModalProps) => {
       <div className="bg-white  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-48 p-5 flex flex-col items-start absolute z-20">
         <h1 className="text-xl font-bold mb-5">CSVファイルをインポート</h1>
         <p className="text-lg mb-5">文字コード:Shift_JIS</p>
+        <input type='file' accept='.csv' className="mb-5" />
         <div className="flex mt-auto w-full">
           <button
             className="bg-slate-900 hover:bg-slate-700 text-white px-8 py-2 mx-auto"
