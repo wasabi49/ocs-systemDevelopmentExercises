77dde714bb776a65b0d6d71ef9777f0a68908e33
wasabi49 <agpamwdjt@icloud.com>
2025-05-21 03:03:05 +0000

    fix: 不要なインポートの削除
    ### Breadcrumbs.test.tsx
    - `usePathname`のインポートを削除しました。
    
    ### page.tsx
    - `Link`のインポートを削除しました。

--- mbs/app/components/Breadcrumbs.test.tsx
+++ mbs/app/components/Breadcrumbs.test.tsx
@@ -2,7 +2,6 @@ import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect, vi } from 'vitest';
 import Breadcrumbs from './Breadcrumbs';
-import { usePathname } from 'next/navigation';
 
 // Next.jsのuseParamsをモック
 vi.mock('next/navigation', () => ({
