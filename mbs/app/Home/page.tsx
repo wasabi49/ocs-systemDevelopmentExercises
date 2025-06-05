027c455c8aa429351752d76d7ba61b13cfce6791
Shuei0209 <s2370196@s.do-johodai.ac.jp>
2025-06-05 04:56:33 +0000

    背景の削除

--- mbs/app/Home/page.tsx
+++ mbs/app/Home/page.tsx
@@ -1,4 +1,29 @@
-const Home = () => {
-  return <h1>ホーム</h1>;
+import React from 'react'
+
+export const Home = () => {
+  const buttons = [
+    { label: '顧客情報一覧', path: '/Home/CustomerList' },
+    { label: '注文一覧', path: '/Home/OrderList' },
+    { label: '納品一覧', path: '/Home/DeliveryList' },
+    { label: '統計情報閲覧', path: '/Home/Statistics' },
+  ];
+
+  return (
+    <div className="min-h-screen flex items-center justify-center  p-4">
+      <div className="w-full max-w-screen-lg grid grid-cols-1 sm:grid-cols-2 sm:gap-15 gap-6">
+        {buttons.map((btn, idx) => (
+          <a
+            key={idx}
+            href={btn.path}
+            className="w-full sm:h-40  h-24 border border-black text-2xl bg-white hover:bg-gray-200 transition flex items-center justify-center"
+          >
+            {btn.label}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 };
+
+
 export default Home;
